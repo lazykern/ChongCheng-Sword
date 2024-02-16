0bbe39efe378fb78ba9ee0ecf849b66130a0c779
Phusit Somboonyingsuk <phusitsom@gmail.com>
2024-02-16 17:20:08 +0700

    espnow woohoo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
+#include "esp_now.h"
+#include "WiFi.h"
+
 
 MPU6050 mpu;
 int16_t ax, ay, az;
@@ -9,10 +12,27 @@ int16_t gx, gy, gz;
 const uint SLASH_COOLDOWN = 500;
 const int16_t DIRECTION_CHANGE_THRESHOLD = 50;
 const int DEBOUNCE_THRESHOLD = 25;
+
 int slash_count = 0;
 
 ulong last_slash = 0;
 ulong last_change_direction = 0;
+
+// 3C:61:05:03:68:74
+uint8_t serverAddress[] = {0x3C, 0x61, 0x05, 0x03, 0x68, 0x74};
+
+esp_now_peer_info_t peerInfo;
+
+// Define a data structure
+typedef struct struct_message {
+  uint8_t swordNumber;
+  char direction;
+  uint8_t action;
+} struct_message;
+
+// Create a structured object
+struct_message myData;
+
 
 enum SwordStates {
   TIP_UP,
@@ -36,10 +56,9 @@ int direction = -1;
 int direction_last = -1;
 int direction_debounce = 0;
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  mpu.initialize();
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void debounce(int now) {
@@ -55,32 +74,73 @@ void debounce(int now) {
   }
 }
 
-void checkSlashDirection() {
-  String slashType;
+void slash(char direction) {
+    Serial.printf("%c %d\n", direction, slash_count++);
+    last_slash = millis();
+
+    myData.action = 1;
+    myData.direction = direction;
+
+    esp_err_t result = esp_now_send(serverAddress, (uint8_t *) &myData, sizeof(myData));
+}
+
+void checkSlash() {
+
+  char slashDirection;
 
   if (direction == TIP_UP) {
-    slashType = "vertical";
+    slashDirection = 'v';
   } else if (direction == TIP_DOWN) {
-    slashType = "vertical";
+    slashDirection = 'v';
   } else if (direction == HAND_UP) {
-    slashType = "horizontal";
+    slashDirection = 'h';
   } else if (direction == HAND_DOWN) {
-    slashType = "horizontal";
+    slashDirection = 'h';
   } else if (direction == TIP_F_NORMAL) {
-    slashType = "vertical";
+    slashDirection = 'v';
   } else if (direction == TIP_F_TWIST) {
-    slashType = "vertical";
+    slashDirection = 'v';
   }
 
-  ulong now = millis();
-
-  if ((now - last_slash > SLASH_COOLDOWN) && gz == 127) {
-    Serial.printf("%s %d\n", slashType, slash_count++);
-    last_slash = now;
+  if ((millis() - last_slash > SLASH_COOLDOWN) && gz == 127) {
+    slash(slashDirection);
   }
 }
 
-void loop() {
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+  
+  memcpy(peerInfo.peer_addr, serverAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  if (WiFi.macAddress().equalsIgnoreCase("E8:68:E7:22:B6:B8")) {
+    myData.swordNumber = 1;
+  } else if (WiFi.macAddress().equalsIgnoreCase("")) {
+    myData.swordNumber = 2;
+  } else {
+    myData.swordNumber = UINT8_MAX;
+  }
+  
+}
+
+void getMotion() {
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
   ax = constrain(ax, -17000, 17000);
@@ -94,6 +154,11 @@ void loop() {
   gx = map(gx, -32768, 32767, -127, 127);
   gy = map(gy, -32768, 32767, -127, 127);
   gz = map(gz, -32768, 32767, -127, 127);
+}
+
+void loop() {
+
+  getMotion();
 
   if (az > DIRECTION_CHANGE_THRESHOLD) {
     debounce(HAND_UP);
@@ -110,5 +175,5 @@ void loop() {
     debounce(TIP_F_TWIST);
   }
 
-  checkSlashDirection();
+  checkSlash();
 }
